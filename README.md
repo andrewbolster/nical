1a03e93b24754b7fcd59460f50f85482573e34ff
Andrew Bolster <me@andrewbolster.info>
2013-12-07 19:22:57 +0000

    mostly there, if the commit works I'll take it live

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ A calendar using for NI's interesting events. Built with [FullCalendar](http://a
 
 Only Flask, Foundation framework is included.
 
+## Get Added
+
+Add your calendar into events.py in the obvious style
+
+If you wish to add custom formatting (colouring etc) to your events, edit the relevant css file under static to access the class name declared in events.py
--- server.py
+++ server.py
@@ -1,27 +1,24 @@
 from flask import Flask, render_template, abort
 from calendar import Calendar
 from datetime import date
+from events import sources
 
 app = Flask(__name__)
 
 defaults = {'year': date.today().year, 'month':date.today().month}
 
 @app.route('/', defaults=defaults)
-@app.route('/<int:year>/')
-@app.route('/<int:year>/<int:month>/')
+@app.route('/<int:year>/', defaults = defaults)
+@app.route('/<int:year>/<int:month>/', defaults = defaults)
 def index(year,month):
-	cal = Calendar(0)
-	try:
-		if not year:
-			year = date.today().year
-		month = date.today().month
-		day = date.today().day
-		cal_list = [cal.monthdatescalendar(year, i+1) for i in xrange(12)]
-	except Exception, e:
-		abort(404)
-	else:
-		return render_template('cal.html', year=year, today = date.today(), cal=cal_list)
-	abort(404)
+    try:
+        day = date.today().day
+        today = date(year,month,day)
+    except Exception, e:
+        abort(404)
+    else:
+        return render_template('cal.html', today = today, sources = sources)
+    abort(404)
 
 if __name__ == '__main__':
     app.run(debug=True)
